b9a7f5ff53119b4f661a280ffd91dcbdf6fe290d
seeduardo <romenflatiron@gmail.com>
2019-03-13 12:16:31 +0000

    Lab completed

--- js/app/controllers/UserController.js
+++ js/app/controllers/UserController.js
@@ -1,11 +1,32 @@
-function UserController() {
+function UserController($scope) {
 	this.username = '';
 
 	this.country = {
 		name: {
-
-		}
+			short: '',
+			full: ''
+		},
+		population: ''
 	};
+
+	$scope.$watch(() => {
+		return this.username
+	}, (nV, oV) => {
+		console.log('username stuff changed, yo: ' + nV)
+	})
+
+	$scope.$watchCollection(() => {
+		return this.country.name
+	}, (nV, oV) => {
+		console.log('specific country name stuff changed, yo: ' + nV)
+	})
+
+	$scope.$watch(() => {
+		return this.country
+	}, (nV, oV) => {
+		console.log('general country stuff changed, yo: ' + nV)
+	}, true)
+
 }
 
 angular
